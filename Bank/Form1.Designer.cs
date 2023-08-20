824c4b30ae158b8221aefbd041cbaddef6164843
GabrielReds13 <speedckprod4007@gmail.com>
2023-08-20 02:45:42 -0300

    Second Commit

---
+++ Bank/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Bank
+{
+    partial class form_login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_login));
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.btn_criarConta = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_recuperarSenha = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.txt_senha = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.txt_conta = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_login)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.White;
+            this.pictureBox6.BackgroundImage = global::Bank.Properties.Resources.Logo_BigSize_Black;
+            this.pictureBox6.Location = new System.Drawing.Point(116, 299);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(95, 52);
+            this.pictureBox6.TabIndex = 24;
+            this.pictureBox6.TabStop = false;
+            // 
+            // btn_criarConta
+            // 
+            this.btn_criarConta.AutoSize = true;
+            this.btn_criarConta.BackColor = System.Drawing.Color.Black;
+            this.btn_criarConta.Font = new System.Drawing.Font("Gotham", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_criarConta.ForeColor = System.Drawing.Color.White;
+            this.btn_criarConta.Location = new System.Drawing.Point(197, 586);
+            this.btn_criarConta.Name = "btn_criarConta";
+            this.btn_criarConta.Size = new System.Drawing.Size(70, 16);
+            this.btn_criarConta.TabIndex = 23;
+            this.btn_criarConta.Text = "Crie uma.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Gotham", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(58, 587);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 15);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Não tem uma conta?";
+            // 
+            // btn_recuperarSenha
+            // 
+            this.btn_recuperarSenha.AutoSize = true;
+            this.btn_recuperarSenha.BackColor = System.Drawing.Color.White;
+            this.btn_recuperarSenha.Font = new System.Drawing.Font("Gotham", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_recuperarSenha.Location = new System.Drawing.Point(183, 476);
+            this.btn_recuperarSenha.Name = "btn_recuperarSenha";
+            this.btn_recuperarSenha.Size = new System.Drawing.Size(87, 16);
+            this.btn_recuperarSenha.TabIndex = 21;
+            this.btn_recuperarSenha.Text = "Clique aqui.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Gotham", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 477);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 15);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Esqueceu a senha?";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(29, 461);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(270, 1);
+            this.pictureBox5.TabIndex = 19;
+            this.pictureBox5.TabStop = false;
+            // 
+            // txt_senha
+            // 
+            this.txt_senha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_senha.Font = new System.Drawing.Font("Gotham", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_senha.Location = new System.Drawing.Point(29, 434);
+            this.txt_senha.Multiline = true;
+            this.txt_senha.Name = "txt_senha";
+            this.txt_senha.Size = new System.Drawing.Size(270, 20);
+            this.txt_senha.TabIndex = 18;
+            this.txt_senha.Text = "Senha";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(29, 403);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(270, 1);
+            this.pictureBox4.TabIndex = 17;
+            this.pictureBox4.TabStop = false;
+            // 
+            // txt_conta
+            // 
+            this.txt_conta.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_conta.Font = new System.Drawing.Font("Gotham", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_conta.Location = new System.Drawing.Point(29, 376);
+            this.txt_conta.Multiline = true;
+            this.txt_conta.Name = "txt_conta";
+            this.txt_conta.Size = new System.Drawing.Size(270, 20);
+            this.txt_conta.TabIndex = 16;
+            this.txt_conta.Text = "Conta";
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackgroundImage = global::Bank.Properties.Resources.Login_Button;
+            this.btn_login.Location = new System.Drawing.Point(29, 522);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(270, 36);
+            this.btn_login.TabIndex = 15;
+            this.btn_login.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(13, 284);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 286);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Bank.Properties.Resources.Background_Wallpaper_Login;
+            this.pictureBox1.Location = new System.Drawing.Point(-7, -28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(340, 424);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // form_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(324, 621);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.btn_criarConta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_recuperarSenha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.txt_senha);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.txt_conta);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(340, 660);
+            this.MinimumSize = new System.Drawing.Size(340, 660);
+            this.Name = "form_login";
+            this.Text = "Bank - Login";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_login)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label btn_criarConta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label btn_recuperarSenha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.TextBox txt_senha;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox txt_conta;
+        private System.Windows.Forms.PictureBox btn_login;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
