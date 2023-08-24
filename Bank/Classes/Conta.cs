eb649f2e5c53cd0ec0b34f75333902ba49bd6f27
GabrielReds13 <speedckprod4007@gmail.com>
2023-08-24 11:47:09 -0300

    Update Archives

--- Bank/Classes/Conta.cs
+++ Bank/Classes/Conta.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Bank.Classes
 {
     internal class Conta
     {
+        // - Atributos -
+        public int Id { get; set; }
+        public int Agencia { get; set; }
+        public double Saldo { get; set; }
+        public double LimiteMensal { get; set; }
+        public string Senha { get; set; }
+
+        // - Metodo Construtor -
+
+
+        // - Metodos -
+        public void Sacar(double valor)
+        {
+            try
+            {
+                // Saque
+                if (valor > Saldo || valor < 1) MessageBox.Show("Valor inválido.");
+                else Saldo -= valor;
+            }
+            catch(Exception ex) 
+            {
+                // Erro
+                MessageBox.Show("Erro ao sacar.");
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            try
+            {
+                // Deposito
+                if (valor < 0) MessageBox.Show("Valor inválido.");
+                else Saldo += valor;
+            }
+            catch (Exception ex)
+            {
+                // Erro
+                MessageBox.Show("Erro ao depositar.");
+            }
+        }
+
+        public void Pix(string chaveRemetente)
+        {
+
+        }
     }
 }
---
+++ Bank/Classes/Endereco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Classes
+{
+    internal class Endereco
+    {
+        // - Atributos -
+        public string EnderecoCompleto { get; set; }
+        public string Cidade { get; set; }
+    }
+}
